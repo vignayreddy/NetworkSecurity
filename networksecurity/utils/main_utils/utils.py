5e7aa834fca3a59997b5b7daeb5d87a74bd1b952
vignay reddy <vignayreddymuduganti@gmail.com>
2025-12-27 14:57:37 +0530

    Final Deployment

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -3,7 +3,6 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import os,sys 
 import numpy as np 
-import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
